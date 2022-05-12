d0ba40108c2c37bc390ab0831b4891218686ae2d
Pavel Gordiyanov <pgordiyanov@slb.com>
2022-05-12 20:04:12 +0600

    submit solution

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gordiyanovp/golang-united-school-homework-4
+
+go 1.17
--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,11 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	errs "github.com/pkg/errors"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +28,48 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.TrimSpace(input)
+	if len(input) == 0 {
+		return "", fmt.Errorf("could not compute the sum: %w", errorEmptyInput)
+	}
+	op1, op2, err := splitIntoOperands(input)
+	if err != nil {
+		//return "", fmt.Errorf("could not compute the sum: %w", err)
+		return "", errs.Wrap(err, "could not compute the sum")
+	}
+
+	i1, err := strconv.Atoi(op1)
+	if err != nil {
+		return "", fmt.Errorf("first operand cannot be converted to int: %w", err)
+	}
+
+	i2, err := strconv.Atoi(op2)
+	if err != nil {
+		return "", fmt.Errorf("second operand cannot be converted to int: %w", err)
+	}
+
+	return strconv.Itoa(i1 + i2), nil
+}
+
+func splitIntoOperands(input string) (op1, op2 string, err error) {
+	tokens := strings.Split(input, "+")
+
+	if len(tokens) > 2 {
+		return "", "", errorNotTwoOperands
+	}
+
+	if len(tokens) == 1 {
+		tokensSub := strings.Split(input, "-")
+
+		if len(tokensSub) > 3 || len(tokensSub) <= 1 {
+			return "", "", errorNotTwoOperands
+		}
+		if len(tokensSub) == 3 {
+			return "-" + strings.TrimSpace(tokensSub[1]), "-" + strings.TrimSpace(tokensSub[2]), nil
+		}
+		return strings.TrimSpace(tokensSub[0]), "-" + strings.TrimSpace(tokensSub[1]), nil
+
+	}
+
+	return strings.TrimSpace(tokens[0]), strings.TrimSpace(tokens[1]), nil
 }
